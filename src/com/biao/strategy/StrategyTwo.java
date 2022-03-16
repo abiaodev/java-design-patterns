ec398452945e1f286611928cde0c632cf52f4be7
DESKTOP-DDP2VLM\abiao <abiaodev@qq.com>
2022-03-17 00:44:06 +0800

    修改策略模式

--- src/com/biao/strategy/StrategyTwo.java
+++ src/com/biao/strategy/StrategyTwo.java
@@ -1,10 +1,13 @@
 package com.biao.strategy;
 
 import com.biao.Logger;
+import com.sun.javafx.css.CalculatedValue;
+import com.sun.org.apache.xpath.internal.operations.Operation;
 
 class StrategyTwo implements Strategy {
     @Override
     public void execute(String name) {
         Logger.LOG("StrategyTwo:" + name);
     }
+    private String name;
 }
